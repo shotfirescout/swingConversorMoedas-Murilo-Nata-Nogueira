190b12d09629ff967550efadf9e9d66c7ad2198f
murilonat nogueira <murilonata376@gmail.com>
2024-08-29 18:19:27 -0300

    Adicionando todos os arquivos do projeto

---
+++ src/main/java/com/uniensinojavaswing/murilo/swingcalculadoraimc/murilo/nata/nogueira/SwingCalculadoraIMCMuriloNataNogueira.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.uniensinojavaswing.murilo.swingcalculadoraimc.murilo.nata.nogueira;
+
+import com.uniensinojavaswing.murilo.swingcalculadoraimc.murilo.nata.nogueira.View.Tela;
+
+/**
+ *
+ * @author muril
+ */
+public class SwingCalculadoraIMCMuriloNataNogueira {
+
+    public static void main(String args[]) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Tela().setVisible(true);
+            }
+        });
+    }
+}
